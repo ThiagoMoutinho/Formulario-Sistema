551709b5f40fed4cff8a8a707b922709307b3d2b
Thiago Moutinho <thiagomoutinho34@gmail.com>
2023-08-10 20:17:28 -0300

    Ajuste da tela

--- src/views/Visualizacao.vue
+++ src/views/Visualizacao.vue
@@ -3,52 +3,52 @@
     <template #titulo>
       Confirmação de Acesso
     </template>
-
-  <v-col class="ml-1">
+  
+    <v-col class="ml-1">
+      <v-row>
+        <v-col cols="12" md="12">
+          <strong>Tipo de Usuario</strong>
+          <p>{{ tipoUsuario }}</p>
+        </v-col>
+        <v-col cols="12" md="6">
+          <strong>Nome</strong>
+          <p>{{ form.nome }}</p>
+        </v-col>
+        <v-col cols="12" md="6">
+          <strong>Orgão</strong>
+          <p>{{ form.orgao }}</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6">
+          <strong>Matricula</strong>
+          <p>{{ form.matricula }}</p>
+        </v-col>
+        <v-col cols="12" md="6">
+          <strong>Cargo</strong>
+          <p>{{ listarCargos }}</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6">
+          <strong>Unidade</strong>
+          <p>{{ form.unidade }}</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6">
+          <strong>Qual foi o sistema Selecionado para ter acesso</strong>
+          <p>{{ form.sistema }}</p>
+        </v-col>
+      </v-row>
+  
+    </v-col>
     <v-row>
-      <v-col cols="12" md="12">
-        <strong>Tipo de Usuario</strong>
-        <p>{{ tipoUsuario }}</p>
-      </v-col>
-      <v-col cols="12" md="6">
-        <strong>Nome</strong>
-        <p>{{ form.nome }}</p>
-      </v-col>
-      <v-col cols="12" md="6">
-        <strong>Orgão</strong>
-        <p>{{ form.orgao }}</p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="6">
-        <strong>Matricula</strong>
-        <p>{{ form.matricula }}</p>
-      </v-col>
-      <v-col cols="12" md="6">
-        <strong>Cargo</strong>
-        <p>{{ form.carregarCargos }}</p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="6">
-        <strong>Unidade</strong>
-        <p>{{ form.unidade }}</p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="6">
-        <strong>Qual foi o sistema Selecionado para ter acesso</strong>
-        <p>{{ form.sistema }}</p>
-      </v-col>
-    </v-row>
-
-  </v-col>
-  <v-row>
-    <v-card-text>
-      <v-card-title>
-        Endereço Informado
-      </v-card-title>
-
+      <v-card-text>
+        <v-card-title>
+          Endereço Informado
+        </v-card-title>
+  
         <v-table>
           <thead>
             <tr>
@@ -71,74 +71,88 @@
         </v-table>
       </v-card-text>
     </v-row>
-
-    <template #botoesFormulario>
-      <v-btn
-        class="mr-4"
-        color="grey"
-        @click="voltar"
-      >
-        Voltar
-      </v-btn>
-
-      <v-btn color="primary">
-        Salvar
-      </v-btn>
-    </template>
-  </CadastroLayout>
-</template>
-<script>
-import CadastroLayout from '@/layouts/default/CadastroLayout.vue';
-import { reactive, computed } from "vue"
-import { useUsuarioStore } from '@/stores/store';
-import { useRouter } from "vue-router";
-
-export default {
-  name: "VisualizarFormulario",
-  components: {
-    CadastroLayout
-  },
-
-  data() {
-    return {
-      useUsuarioStore: useUsuarioStore(),
-      listarCargos:[],
-    }
-  },
-
-// === data === //
-
-const { form, tela } = useUsuarioStore()
-const router = useRouter();
-
-// === end data
-
-// === computed === //
-
-    const tipoUsuario = computed(() => {
-      if (typeof tela.tipoUsuario === 'undefined') {
-        return;
-      }
-
-  for(let i = 0; i < tela.tipoUsuario.length; i++) {
-    if(tela.tipoUsuario[i].valor == form.tipoUsuario) {
-      return tela.tipoUsuario[i].nome;
-    }
-  }
-  return ''
-})
-
-
-// === end computed === //
-
-// === methods ===
-
-const voltar = () => {
-  router.push('/')
-}
-
-
   
-};
-
-</script>
+      <template #botoesFormulario>
+        <v-btn
+          class="mr-4"
+          color="grey"
+          @click="voltar"
+        >
+          Voltar
+        </v-btn>
+  
+        <v-btn color="primary">
+          Salvar
+        </v-btn>
+      </template>
+  </CadastroLayout>
+  
+  
+  
+  </template>
+  <script>
+  import {  computed } from 'vue';
+  import CadastroLayout from "@/layouts/default/CadastroLayout.vue"
+  import { useUsuarioStore } from '@/stores/store';
+  import axios from 'axios';
+  
+  export default {
+    name: "VisualizarFormulario",
+    components: {
+      CadastroLayout
+    },
+  
+    data() {
+      return {
+        useUsuarioStore: useUsuarioStore(),
+        listarCargos:[],
+      }
+    },
+  
+    setup() {
+      const { form, tela } = useUsuarioStore();
+  
+      const tipoUsuario = computed(() => {
+        if (typeof tela.tipoUsuario === 'undefined') {
+          return;
+        }
+  
+        for (let i = 0; i < tela.tipoUsuario.length; i++) {
+          if (tela.tipoUsuario[i].valor === form.tipoUsuario) {
+            return tela.tipoUsuario[i].nome;
+          }
+        }
+        return '';
+      });
+      return {
+        tipoUsuario,
+        form,
+        tela,
+      };
+    },
+  
+    mounted() {
+      this.carregarCargos()
+    },
+  
+    methods: {
+      voltar() {
+        this.$router.push("/")
+      }, 
+  
+      carregarCargos() {
+        axios.get(`https://homologacao.policiacivil.pa.gov.br/teste-thiago/public/api/cargos/`)
+        .then(response => {
+          this.listarCargos = response.data;
+        })
+        .catch(error => {
+          console.log('Erro ao buscar os nomes da API:', error);
+        });
+      },
+    },
+  
+  
+    
+  };
+  
+  </script>
